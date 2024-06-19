ee876a890e8d08cd38c492d228d15494300b4c99
Guilherme Yonamine <guile.yonamine@gmail.com>
2024-06-19 16:38:53 -0300

    Token API

--- tabela_fipe_api/src/services/api.js
+++ tabela_fipe_api/src/services/api.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://parallelum.com.br/fipe/api/v1',
-  // baseURL: 'http://api.fipeapi.com.br/v1',
+  headers: {
+    'X-Subscription-Token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJiNDcxMjRjOS00NzA1LTRjYTktOTY2Yi1lM2YyZWQxZTc0NTMiLCJlbWFpbCI6Imd1aWxlLnlvbmFtaW5lQGdtYWlsLmNvbSIsImlhdCI6MTcxODgxODA0MH0.nGLvtMs-EGvpm57AoE4RZlXWb1euAVtcMK7KsXjNnBU'  // Adicione seu token aqui
+  }
 });
 
 export default api;
